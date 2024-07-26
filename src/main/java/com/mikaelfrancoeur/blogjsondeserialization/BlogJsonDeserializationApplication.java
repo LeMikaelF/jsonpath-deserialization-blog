f4eb86d17b0b7043308bdfe2c8744a9a3ed546ba
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2024-07-25 21:25:52 -0400

    initial commit

---
+++ src/main/java/com/mikaelfrancoeur/blogjsondeserialization/BlogJsonDeserializationApplication.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.blogjsondeserialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJsonDeserializationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogJsonDeserializationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikaelfrancoeur/blogjsondeserialization/BlogJsonDeserializationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.blogjsondeserialization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJsonDeserializationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
